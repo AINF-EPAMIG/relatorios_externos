d8059c39da069d2624cd5043b170c17b02c26108
Rodolfo <rodolfo-fernandes-@hotmail.com>
2025-05-12 16:45:37 -0300

    teste 8.7

--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { Globe, Mail, LayoutDashboard } from "lucide-react"
+import { Globe } from "lucide-react"
 import Image from 'next/image'
 
 export function DashboardHeader() {
