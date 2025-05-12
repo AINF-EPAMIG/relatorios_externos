af046fa93315d6102969bd9526f23c75f7417643
Rodolfo <rodolfo-fernandes-@hotmail.com>
2025-05-12 15:08:18 -0300

    teste 1

--- app/api/contratos/relatorio/[tipo]/route.ts
+++ app/api/contratos/relatorio/[tipo]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { Contratos } from '@/modules/contratos/db'
+import { Contratos } from '@/lib/db'
 
 export async function GET(req: NextRequest, { params }: { params: { tipo: string } }) {
   try {
